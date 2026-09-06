d0ed5510a9e78146f4a72c35fe8a02ae77273226
KaoKsn <mayurakrishna.25@proton.me>
2026-09-06 17:46:31 +0530

    add lsp.lua: lsp plugin configurations

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,51 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"clangd", -- C/C++
+					"bashls", -- Bash
+					"lua_ls", -- Lua
+					"pyright", -- Python
+					"jdtls", -- Java
+					"ts_ls", -- JavaScript / TypeScript
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+		config = function()
+			-- 1. Global fallback or default options for all servers if needed
+			vim.lsp.config("*", {
+				root_markers = { ".git" },
+			})
+
+			-- 2. Specific overrides (e.g., Lua global `vim` warning fix)
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+
+			-- 3. Enable all servers so they auto-start when opening matching filetypes
+			local servers = { "clangd", "bashls", "lua_ls", "pyright", "jdtls", "ts_ls" }
+			for _, server in ipairs(servers) do
+				vim.lsp.enable(server)
+			end
+		end,
+	},
+}
